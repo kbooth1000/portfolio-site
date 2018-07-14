7ee29280b5e993cd0a2e3adb5017ae26d8d8edc1
Kyle Booth <kbooth1000@gmail.com>
2018-07-13 21:27:45 -0400

    Try netlify form4

--- src/components/ContactFormA.js
+++ src/components/ContactFormA.js
@@ -4,7 +4,7 @@ import './css/contact.css';
 // import './ContactForm.css'
 
 const ContactFormA = () => (
-  <form className="contact-form" name='contact-form' data-netlify="true" method="POST">
+  <form className="contact-form" name='contact-form' data-netlify="true">
     <div className="form-field">
       <label htmlFor="name">
         <div className="label-content">Name:</div>
