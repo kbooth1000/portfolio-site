f5053556d3cc2c1ddec8432b79f098a4b8982a95
Kyle Booth <kbooth1000@gmail.com>
2018-07-17 03:21:02 -0400

    Change app.get in server to redirect to root

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.redirect(
     '/'
   );
