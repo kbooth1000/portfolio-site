775556f98fe19d12f1ad876053b817a946864d34
Kyle Booth <kbooth1000@gmail.com>
2018-08-04 01:26:52 -0400

    Made a number of css changes

--- server/client/src/components/About.js
+++ server/client/src/components/About.js
@@ -21,14 +21,17 @@ let About = (props) => {
     <p>
     Hello, my name is Kyle Booth. I am an Atlanta-based web developer and designer working with mostly JavaScript-based Front and Back-End technologies (and some PHP for Wordpress theme development). 
     </p><p>
-    I've been working with JavaScript, HTML and CSS for over 10 years and recently graduated from a super-intense full-time coding bootcamp (<a href="https://www.digitalcrafts.com/immersive-web.html" target="_new">Digital Crafts Full-Stack Immersive</a>), where I ramped up my <FontAwesomeIcon icon={['fab', 'js-square']} style={{color: '#ffc000'}} />ES6, <FontAwesomeIcon icon={['fab', 'react']} style={{color: 'blue'}} />React, <FontAwesomeIcon icon={['fab', 'node-js']} style={{color: 'green'}} />Node, et al skills.
+    I've been working with JavaScript, HTML and CSS for over 10 years and recently graduated from a super-intense full-time coding bootcamp (<a href="https://www.digitalcrafts.com/immersive-web.html" target="_new">Digital Crafts Full-Stack Immersive</a>), where I ramped up my <br/> <FontAwesomeIcon icon={['fab', 'js-square']} style={{color: '#ffc000'}} />ES6, <FontAwesomeIcon icon={['fab', 'react']} style={{color: 'blue'}} />React, <FontAwesomeIcon icon={['fab', 'node-js']} style={{color: 'green'}} />Node,<br/> et al skills.
     </p><p>
     Now, as I continue to dig deeper into these awesome technologies, I'm looking for a company to work with where I can help them build great stuff!
     </p><p>
       When I'm not coding, I love exploring the city and being out around people. From the Beltline to Buford Highway and from the West End to the International Farmers' Market, there's always something cool to discover.
       </p><p>
-        If you'd like to <Link to="/contact" onClick={()=>handleClick('content-page contact')} style={{borderBottom:"2px #dfdfdf solid"}}>get in touch with me</Link>, I'd love to hear from you.
-      </p>
+        If you'd like to <br/><Link to="/contact" onClick={()=>handleClick('content-page contact')} style={{borderBottom:"2px solid rgb(213, 213, 213)",
+    background: "rgb(236, 236, 236)",
+    borderRadius: "5px",
+    padding: ".1rem .4rem"}}>get in touch with me</Link>,<br/> I'd love to hear from you.
+      </p> <br/><br/><br/><br/>
     </div>
   )
 }
