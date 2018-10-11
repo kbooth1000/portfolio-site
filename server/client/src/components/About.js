b65c09fd3be1aa9f580c3b9ab5dd6557b6415a6d
Kyle Booth <kbooth1000@gmail.com>
2018-10-11 12:46:14 -0400

    Remove node_modules from git tracking

--- server/client/src/components/About.js
+++ server/client/src/components/About.js
@@ -5,36 +5,78 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
 
-import { setHeaderClass} from '../actions/actions';
+import { setHeaderClass } from '../actions/actions';
 import { connect } from 'react-redux';
 
 library.add(fab, faCheckSquare);
 
-let About = (props) => {
-
-  let handleClick = (projectkey) => {
+let About = props => {
+  let handleClick = projectkey => {
     props.setMainClass(projectkey);
- }
+  };
 
   return (
-    <div style={{ width:"80%", textAlign:"center", maxWidth:"750px" }}>
-    <p>
-    Hello, my name is Kyle Booth. I am an Atlanta-based web developer and designer working with mostly JavaScript-based Front and Back-End technologies (and some PHP for Wordpress theme development). 
-    </p><p>
-    I've been working with JavaScript, HTML and CSS for over 10 years and recently graduated from a super-intense full-time coding bootcamp (<a href="https://www.digitalcrafts.com/immersive-web.html" target="_new">Digital Crafts Full-Stack Immersive</a>), where I ramped up my <br/> <FontAwesomeIcon icon={['fab', 'js-square']} style={{color: '#ffc000'}} />ES6, <FontAwesomeIcon icon={['fab', 'react']} style={{color: 'blue'}} />React, <FontAwesomeIcon icon={['fab', 'node-js']} style={{color: 'green'}} />Node,<br/> et al skills.
-    </p><p>
-    Now, as I continue to dig deeper into these awesome technologies, I'm looking for a company to work with where I can help them build great stuff!
-    </p><p>
-      When I'm not coding, I love exploring the city and being out around people. From the Beltline to Buford Highway and from the West End to the International Farmers' Market, there's always something cool to discover.
-      </p><p>
-        If you'd like to <br/><Link to="/contact" onClick={()=>handleClick('content-page contact')} style={{borderBottom:"2px solid rgb(213, 213, 213)",
-    background: "rgb(236, 236, 236)",
-    borderRadius: "5px",
-    padding: ".1rem .4rem"}}>get in touch with me</Link>,<br/> I'd love to hear from you.
-      </p> <br/><br/><br/><br/>
+    <div style={{ width: '80%', textAlign: 'center', maxWidth: '750px' }}>
+      <p>
+        Hello, my name is Kyle Booth. I am an Atlanta-based web developer and
+        designer working with mostly JavaScript-based Front and Back-End
+        technologies (and some PHP for Wordpress theme development).
+      </p>
+      <p>
+        I have quite a bit of experience with JavaScript, HTML and CSS and I
+        recently graduated from a super-intense full-time coding bootcamp (
+        <a
+          href="https://www.digitalcrafts.com/immersive-web.html"
+          target="_new"
+        >
+          Digital Crafts Full-Stack Immersive
+        </a>
+        ), where I ramped up my <br />{' '}
+        <FontAwesomeIcon
+          icon={['fab', 'js-square']}
+          style={{ color: '#ffc000' }}
+        />
+        ES6,{' '}
+        <FontAwesomeIcon icon={['fab', 'react']} style={{ color: 'blue' }} />
+        React,{' '}
+        <FontAwesomeIcon icon={['fab', 'node-js']} style={{ color: 'green' }} />
+        Node,
+        <br /> et al skills.
+      </p>
+      <p>
+        Now, as I continue to dig deeper into these awesome technologies, I'm
+        looking for a company to work with where I can help them build great
+        stuff!
+      </p>
+      <p>
+        When I'm not coding, I love exploring the city and being out around
+        people. From the Beltline to Buford Highway and from the West End to the
+        International Farmers' Market, there's always something cool to
+        discover.
+      </p>
+      <p>
+        If you'd like to <br />
+        <Link
+          to="/contact"
+          onClick={() => handleClick('content-page contact')}
+          style={{
+            borderBottom: '2px solid rgb(213, 213, 213)',
+            background: 'rgb(236, 236, 236)',
+            borderRadius: '5px',
+            padding: '.1rem .4rem'
+          }}
+        >
+          get in touch with me
+        </Link>
+        ,<br /> I'd love to hear from you.
+      </p>{' '}
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
-  )
-}
+  );
+};
 
 let mapDispatchToProps = {
   setMainClass: setHeaderClass
@@ -43,7 +85,10 @@ let mapDispatchToProps = {
 let mapStateToProps = state => {
   return {
     headerClass: state.layout.headerClass
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(About);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(About);
