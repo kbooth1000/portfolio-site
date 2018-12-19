4517cc2c6bb88c98880eee3b5310b08757502eae
Kyle Booth <kbooth1000@gmail.com>
2018-12-19 13:51:15 -0500

    mailer fix 3

--- server/mailer/index.js
+++ server/mailer/index.js
@@ -2,20 +2,6 @@ var nodemailer = require('nodemailer');
 const xoauth2 = require('xoauth2');
 var smtpTransport = require('nodemailer-smtp-transport');
 
-// var transporter = nodemailer.createTransport(smtpTransport({
-//   service: 'gmail',
-//   auth: {
-//     xoauth2: xoauth2.createXOAuth2Generator({
-//       user: 'kbooth1000@gmail.com',
-//       clientId: '413926680678-m355i7glm7ijhdevsgej6d9d5rntqas7.apps.googleusercontent.com',
-//       pass: 'strongishCaliLoo0',
-//       clientSecret: 'h_e6AhXU8oGqc27izP1MDhyQ',
-//       refreshToken: '1/QuLrEys1Mi9RUG1TnzK1zkpUv7M3aWofsIxQ7_ohzlc',
-//       accessToken: 'ya29.Glv1BU1lweKfwp4cWijPGlRHHClFmLoiG1AA_tYN2ftgLIl0yU93uL4QQJtpbzujLHNku25qp6CEYTllXjimp_Yw85zC14M0l9oplmaLixcQ-bNO7EBqYvalVq83'
-//     })
-//   }
-// }));
-
 let transporter = nodemailer.createTransport({
   host: 'mail.boothwebproduction.com',
   port: 465,
