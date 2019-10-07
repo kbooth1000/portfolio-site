91d453cc69fddf99654a4fb1b480d00a3242c077
Kyle Booth <kbooth1000@gmail.com>
2019-10-07 15:00:37 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,8 +7,8 @@ let transporter = nodemailer.createTransport({
     xoauth2: xoauth2.createXOAuth2Generator({
       user: 'kbooth1000@gmail.com',
       clientId: '413926680678-m355i7glm7ijhdevsgej6d9d5rntqas7.apps.googleusercontent.com',
-      pass: 'strongishCaliLoo0',
-      clientSecret: 'h_e6AhXU8oGqc27izP1MDhyQ',
+      pass: '',
+      clientSecret: '',
       refreshToken: '1/TjqeALCnZgUxXfcDu6qtvEy7eckIVXeSszRQCLXZoYo',
       accessToken: 'ya29.Glv1BYXHCdPu0ofOsnzn2_a45RZDb-rGcnRQp7r_2L2fe9jjg1-hQSFD7c2g5s6oHeauQ7YbY63koO2CZFnZCTsewuowPbUd3D3UUl6zsjXj47EIQm-EqCWeG7M-'
     })
